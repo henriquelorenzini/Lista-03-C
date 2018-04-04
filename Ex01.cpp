4969d9394638a43f35228f6ec1cbdaefee896f4a
castilh0s <35281738+castilh0s@users.noreply.github.com>
2018-04-03 21:46:52 -0300

    Update Ex01.cpp
    Indentação do código.

--- Ex01.cpp
+++ Ex01.cpp
@@ -1,66 +1,51 @@
-#include <stdio.h>
-#include <locale.h>
-#include <string.h>
-#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<string.h>
 
 main () {
+	setlocale(LC_ALL, "Portuguese");
+	
 	char palavra[200];
-	int caracteres,
-		vogais,
-		consoantes,
-		numeros,
-		espacos,
-		i,
-		fim;
-	
-		vogais = 0;
-		consoantes = 0;
-		numeros = 0;
-		espacos = 0;
-	
+	int caracteres = 0,
+	    vogais     = 0,
+	    consoantes = 0,
+	    numeros    = 0,
+	    espacos    = 0,
+	    i          = 0,
+	    fim        = 0;
+	    vogais     = 0;
+	    consoantes = 0;
+	    numeros    = 0;
+	    espacos    = 0;
 	
 	printf("Digite uma frase ou palavra: ");
 	gets(palavra);
 	caracteres = strlen(palavra);
-	printf("Quantidade de caracteres = %d\n", caracteres);
+	printf("\nQuantidade de caracteres = %d\n", caracteres);
+	
 	fim = caracteres - 1;
 	
-	for(i = 0; i <= fim; i++) {
-		if(palavra[i]=='a' || palavra[i]=='e' || palavra[i]=='i' ||
-           palavra[i]=='o' || palavra[i]=='u' || palavra[i]=='A' ||
-           palavra[i]=='E' || palavra[i]=='I' || palavra[i]=='O' ||
-           palavra[i]=='U')
-        {
+	for(i = 0; i <= fim; i++){
+		if(palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' ||palavra[i] == 'o' || palavra[i] == 'u' || palavra[i] == 'A' || palavra[i] == 'E' || palavra[i] == 'I' || palavra[i] == 'O' || palavra[i] == 'U'){
         	++vogais;
-
-	    }
-		else if ((palavra[i]>='a'&& palavra[i]<='z') || (palavra[i]>='A'&& palavra[i]<='Z'))
-	    {
-
-	    	++consoantes;
-
-	    }
-	    if (palavra[i]>='0' && palavra[i]<='9')
-	   
-	    {
-	    	++numeros;
-	   
-	    }
-	    if (palavra[i]==' ')
-	    {
-	    
-			++espacos;
-	    
+		}else if ((palavra[i] >= 'a' && palavra[i] <= 'z') || (palavra[i] >= 'A' && palavra[i] <= 'Z')){
+			++consoantes;
 		}
+		
+		if (palavra[i] >= '0' && palavra[i] <= '9'){
+	    	++numeros;
+	   	}
 	    
+		if (palavra[i] == ' '){
+	    	++espacos;
+	    }
 	}
-	printf("Quantidade de vogais     = %d\n", vogais);
 	
+	printf("Quantidade de vogais = %d\n", vogais);
 	printf("Quantidade de consoantes = %d\n", consoantes);
-	
-	printf("Quantidade de numeros    = %d\n", numeros);
-	
-	printf("Quantidade de espacos    = %d\n", espacos);
+	printf("Quantidade de números = %d\n", numeros);
+	printf("Quantidade de espacos = %d\n", espacos);
 	
 	return 0;
 }
